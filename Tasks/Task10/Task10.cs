1765ea2fcde9693c97216ad1dd7e6ce08a00e3ca
KirillVarnavskiy <k.varnavskiy@mail.ru>
2022-12-14 00:18:01 +0300

    Hometask2 done

---
+++ Tasks/Task10/Task10.cs
@@ -0,0 +1,6 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Enter a three-digit number: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(Number);
+Console.WriteLine($"The second digit of {Number}] is "+stringNumber[1]);
---
+++ Tasks/Task15/Task15.cs
@@ -0,0 +1,12 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Enter the number representing the day of the week: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("this day is weekend");
+  }
+  else Console.WriteLine("this day is not weekend");
+}
+CheckingTheDayOfTheWeek(dayNumber);
