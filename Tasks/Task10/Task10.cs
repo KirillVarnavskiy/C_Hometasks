9a20651b907dc570a72bdbeaf5e1d5d0a49528fc
KirillVarnavskiy <k.varnavskiy@mail.ru>
2022-12-15 00:21:03 +0300

    Hometask2 corrected

--- Tasks/Task10/Task10.cs
+++ Tasks/Task10/Task10.cs
@@ -1,6 +1,7 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
 Console.Write("Enter a three-digit number: ");
-int Number = Convert.ToInt32(Console.ReadLine());
-string stringNumber = Convert.ToString(Number);
-Console.WriteLine($"The second digit of {Number}] is "+stringNumber[1]);
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = n/10;
+int n2 = n1 % 10;
+Console.WriteLine($"The second digit of {n} is "+n2);
--- Tasks/Task13/Task13.cs
+++ Tasks/Task13/Task13.cs
@@ -1,11 +1,13 @@
 ﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
 Console.Write("Enter the number: ");
-int Number = Convert.ToInt32(Console.ReadLine());
-string NumberText = Convert.ToString(Number);
-if (NumberText.Length > 2){
-  Console.WriteLine($"The third digit of {Number} is " + NumberText[2]);
-}
-else {
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 100){
   Console.WriteLine("The third digit is absent");
+}
+else 
+{while (n > 1000)
+        n = n / 10;
+
+  Console.WriteLine(n % 10);
 }
