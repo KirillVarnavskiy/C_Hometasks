075d2d416691f686c22bfacc8e7f9a7a0d4d9e2f
KirillVarnavskiy <k.varnavskiy@mail.ru>
2022-12-18 18:57:13 +0300

    Hometask3 done

---
+++ Tasks/Task19/Task19.cs
@@ -0,0 +1,21 @@
+﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("Enter a five-digit number: ");
+string? n = Console.ReadLine();
+int n1 = 0;
+// if (n!.Length != 5)
+// Console.WriteLine("Entered number is not five-digit one, but");
+
+for (int i = n.Length-1; i >= 0; i--)
+{  
+    n1 = n1 + Convert.ToInt32(int.Parse(n[i].ToString()) * Math.Pow(10, i));
+}
+
+if (n1 == int.Parse(n) && n!.Length == 5)
+Console.WriteLine($"yes, number {n} is palindrome");
+
+else if (n1 == int.Parse(n) && n!.Length != 5)
+Console.WriteLine($"number {n} is palindrome, but not five-digit one");
+
+else
+Console.WriteLine($"no, number {n} is not palindrome");
---
+++ Tasks/Task21/Task21.cs
@@ -0,0 +1,13 @@
+﻿// Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Enter the coordinates of point A");
+double X1 = Convert.ToDouble(Console.ReadLine());
+double Y1 = Convert.ToDouble(Console.ReadLine());
+double Z1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter the coordinates of point B");
+double X2 = Convert.ToDouble(Console.ReadLine());
+double Y2 = Convert.ToDouble(Console.ReadLine());
+double Z2 = Convert.ToDouble(Console.ReadLine());
+
+double result = Math.Sqrt((Math.Pow(X1-X2,2))+Math.Pow(Y1-Y2,2)+Math.Pow(Z1-Z2,2));
+Console.WriteLine($"The distance between points; {Math.Round(result, 2)}");
