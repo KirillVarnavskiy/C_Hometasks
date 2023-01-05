96452fd5703036dd4feab031428a74cf0ea39cda
KirillVarnavskiy <k.varnavskiy@mail.ru>
2023-01-05 18:56:23 +0300

    Hometask8 done

---
+++ Tasks/Task56/Task56.cs
@@ -0,0 +1,68 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Console.WriteLine($"\nEnter the size m x n of a rectangular two-dimensional array (m must not be equal to n):");
+int m = InputNumbers("Enter m: ");
+int n = InputNumbers("Enter n: ");
+
+int[,] matrix = new int[m, n];
+CreateArray(matrix);
+WriteArray(matrix); 
+
+int minSumLine = 0;
+int sumLine = SumLineElements(matrix, 0);
+for (int i = 1; i < matrix.GetLength(0); i++)
+{
+  int tempSumLine = SumLineElements(matrix, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+
+Console.WriteLine($"\nThe row with the lowest-sum is {minSumLine+1}, element's sum is {sumLine}");
+
+
+int SumLineElements(int[,] matrix, int i)
+{
+  int sumLine = matrix[i,0];
+  for (int j = 1; j < matrix.GetLength(1); j++)
+  {
+    sumLine += matrix[i,j];
+  }
+  return sumLine;
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = new Random().Next(1, 11); // [1, 10];
+    }
+  }
+}
+
+void WriteArray (int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write($"{matrix[i, j]} \t");
+    }
+    Console.WriteLine();
+  }
+}
+
+
+
+
---
+++ Tasks/Task62/Task62.cs
@@ -0,0 +1,36 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Write("Enter the size of the array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[size, size];
+int num = 1;
+int i = 0;
+int j = 0;
+
+while (num <= size * size)
+{
+    matrix[i, j] = num;
+    if (i <= j + 1 && i + j < size - 1)
+        j++;
+    else if (i < j && i + j >= size - 1)
+        i++;
+    else if (i >= j && i + j > size - 1)
+        j--;
+    else
+        i--;
+    num++;
+}
+PrintMatrix(matrix);
