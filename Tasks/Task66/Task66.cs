297ed40030e574b2c49286334a98e01fa991ec2d
KirillVarnavskiy <k.varnavskiy@mail.ru>
2023-01-09 17:45:10 +0300

    Hometask9 done

---
+++ Tasks/Task66/Task66.cs
@@ -0,0 +1,31 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int InputNumber(string input)
+{
+     Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+ }
+
+ void RecSum(int m, int n, int sum)
+ {
+     sum = sum + n;
+     if (n <= m)
+    {
+         Console.Write($" {sum} ");
+         return;
+     }
+     RecSum(m, n - 1, sum);
+
+ }
+ int m = InputNumber("m: ");
+ int n = InputNumber("n: ");
+ int temp = m;
+
+ if (m > n)
+ {
+     m = n;
+     n = temp;
+ }
+ Console.Write($"{m}, {n} ->");
+ RecSum(m, n, temp = 0);
